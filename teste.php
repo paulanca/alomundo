6fff411132baa1c13a9c97215e3e5269dc408474
paulanca <54368566+paulanca@users.noreply.github.com>
2019-08-21 14:40:07 -0300

    Update teste.php

--- teste.php
+++ teste.php
@@ -1,5 +1,9 @@
-<form action="/action_page.php" method="get">
-  First name: <input type="text" name="fname"><br>
-  Last name: <input type="text" name="lname"><br>
-  <input type="submit" value="Submit">
-</form>
+<htm>
+  <body>
+    <form action="/teste.php" method="get">
+      First name: <input type="text" name="fname"><br>
+      Last name: <input type="text" name="lname"><br>
+      <input type="submit" value="Submit">
+    </form>
+  </body>
+</htm>

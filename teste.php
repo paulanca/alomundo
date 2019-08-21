fd68831763b0697642d15aeb85118630682a0491
paulanca <54368566+paulanca@users.noreply.github.com>
2019-08-21 14:41:18 -0300

    Delete teste.php

--- teste.php
+++
@@ -1,9 +0,0 @@
-<htm>
-  <body>
-    <form action="/teste.php" method="get">
-      First name: <input type="text" name="fname"><br>
-      Last name: <input type="text" name="lname"><br>
-      <input type="submit" value="Submit">
-    </form>
-  </body>
-</htm>
